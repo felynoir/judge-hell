fe966424d9c49a39543b23554c88cddcb6e25a81
Dragonfly161518 <tatsuya10199@gmail.com>
2019-05-11 19:29:21 +0700

    set prod for dotenv.config

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
-import dotenv from 'dotenv';
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
 import errorHandler from './error-handler';
 import config from './config';
 
