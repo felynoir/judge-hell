1682f00ac0b55110b278fbd3a5a3a24b8098c962
dragonfly161518 <tatsuya10199@gmail.com>
2019-06-08 19:37:33 +0700

    Debug

--- src/controllers/problem/new-problem.js
+++ src/controllers/problem/new-problem.js
@@ -8,7 +8,9 @@ export default ({ Problem }) => async (req, res, next) => {
 
     console.log('new file', req.file);
     // const problem = await Problem.create(data)
-
+    console.log('======================');
+    console.log(__dirname);
+    console.log(__filename);
     const options = {
       destination: `${problemName}/problem.pdf`,
       gzip: true,
@@ -16,9 +18,8 @@ export default ({ Problem }) => async (req, res, next) => {
         cacheControl: 'public, max-age=31536000',
       },
     };
-
     await Bucket.upload(req.file.path, options);
-
+    console.log('success');
     return res.status(200).json('problem');
   } catch (e) {
     next(e);
