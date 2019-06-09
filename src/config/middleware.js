ec3cd1e7459973bf171b6eb142f0623d0feb34fb
dragonfly161518 <tatsuya10199@gmail.com>
2019-06-10 04:46:47 +0700

    Add securing kue

--- src/config/middleware.js
+++ src/config/middleware.js
@@ -2,6 +2,7 @@ import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import passport from '../passport';
 import cors from 'cors';
+import kue from 'kue';
 
 const isTest = process.env.NODE_ENV === 'test';
 const isDev = process.env.NODE_ENV === 'development';
@@ -11,6 +12,7 @@ export default app => {
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(passport.init());
   app.use(cors());
+  app.use(kue.app);
 
   if (isDev && !isTest) {
     app.use(morgan('dev'));
